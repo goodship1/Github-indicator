498bffa0f4fbaca5fa8f2b859ccf7f6737272ba4
goodship1 <msgoodship1@gmail.com>
2019-02-10 20:58:06 +0000

    got bar sort of working

--- mainwindow.py
+++ mainwindow.py
@@ -1,17 +1,36 @@
 import gi
+import os
+from GithubInformation import GithubInformation
 gi.require_version('Gtk', '3.0')
 gi.require_version('AppIndicator3', '0.1')
 from gi.repository import Gtk as gtk
 from gi.repository import AppIndicator3 as appindicator
-import signal 
+ 
+
 
 class GithubInfobar(object):
 	
-	  APPINDICATOR_ID = "GithubInformation"
-	  
-	  def __init__(self):
-		  self.indicator = appindicator.Indicator.new(APPINDICATOR_ID)
-		  
+		
+	    
+		def __init__(self):
+		    self.indicator = appindicator.Indicator.new("githubapplication" , os.path.dirname(os.path.realpath(__file__))+'/image/github.jpg',appindicator.IndicatorCategory.APPLICATION_STATUS)
+		    self.indicator.set_status(appindicator.IndicatorStatus.ACTIVE)
+		    self.indicator.set_menu(gtk.Menu())
+		    
+		    
+		
+		
+		def menu(self):
+			pass
+		    
+	
+		def main(self):
+			gtk.main()
 	
 	
-	
+		
+
+
+
+Git = GithubInfobar()
+Git.main()
