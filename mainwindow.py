2739d76990f9cb5801b983ef6873dce15dd1c31d
goodship1 <msgoodship1@gmail.com>
2019-02-11 21:20:11 +0000

    writing github information function

--- mainwindow.py
+++ mainwindow.py
@@ -16,15 +16,20 @@ class GithubInfobar(object):
 		    self.indicator = appindicator.Indicator.new("githubapplication" , os.path.dirname(os.path.realpath(__file__))+'/image/github.jpg',appindicator.IndicatorCategory.APPLICATION_STATUS)
 		    self.indicator.set_status(appindicator.IndicatorStatus.ACTIVE)
 		    self.indicator.set_menu(gtk.Menu())
-		    
+		
+		
+		def _github_Info(self):
+			git = GithubInformation()
+			repos = git.get_Public_repository
+			stars = git.get_Stars()
+			followers = git.get_Followers()
+			return (repos,stars,followers)
 		    
 		
 		
-		def menu(self):
-			pass
-		    
-	
-		def main(self):
+		
+		@staticmethod
+		def main():
 			gtk.main()
 	
 	
@@ -33,4 +38,4 @@ class GithubInfobar(object):
 
 
 Git = GithubInfobar()
-Git.main()
+
