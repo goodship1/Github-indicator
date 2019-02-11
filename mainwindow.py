2fb33bb414b7a18bda21fc2b5dbf643c72709f04
goodship1 <msgoodship1@gmail.com>
2019-02-11 22:10:59 +0000

    adding get information to github infobar

--- mainwindow.py
+++ mainwindow.py
@@ -16,15 +16,34 @@ class GithubInfobar(object):
 		    self.indicator = appindicator.Indicator.new("githubapplication" , os.path.dirname(os.path.realpath(__file__))+'/image/github.jpg',appindicator.IndicatorCategory.APPLICATION_STATUS)
 		    self.indicator.set_status(appindicator.IndicatorStatus.ACTIVE)
 		    self.indicator.set_menu(gtk.Menu())
+		    self.public_Repositories  = self.repositories
+		    self.total_Stars = self.stars
+		    self.total_Followers = self.followers
+			 
+		 
 		
 		
 		def _github_Info(self):
 			git = GithubInformation()
-			repos = git.get_Public_repository
+			repos = git.get_Public_repository()
 			stars = git.get_Stars()
 			followers = git.get_Followers()
 			return (repos,stars,followers)
-		    
+		
+		@property
+		def repositories(self):
+			repos = self._github_Info()
+			return str(repos[0])
+		
+		@property
+		def stars(self):
+			stars = self._github_Info()
+			return str(stars[1])
+		
+		@property
+		def followers(self):
+			followers = self._github_Info()
+			return str(followers[2])
 		
 		
 		
