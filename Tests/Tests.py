cbf6817e4c613b846514bcd065ea52eb3c63daa1
goodship1 <msgoodship1@gmail.com>
2019-02-12 20:20:15 +0000

    writing more tests

--- Tests/Tests.py
+++ Tests/Tests.py
@@ -1,10 +1,24 @@
 import pytest
 import GithubInformation
+import mainwindow
 
 Git = GithubInformation()
 
+mainWindow = GithubInfobar()
 
-def testing_Of_get_Star():
+
+
+def testing_Mainwindow_repositories():
+    assert(type(mainWindow.repositories))==int
+
+
+def testing_Mainwindow_stars():
+    assert(type(mainWindow.stars))==int
+
+def testing_Mainwindow_followers():
+    assert(type(mainWindow.followers))==int
+
+def  testing_Of_get_Star():
     assert(type(git.get_stars())==int
 
 
